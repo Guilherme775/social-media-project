d31a382e50667c1843b50cd5721bc1c0ef017aae
Guilherme775 <guilhermereis28122002@gmail.com>
2021-04-27 18:00:16 -0300

    feat: add relay structure

---
+++ src/RelayEnv.ts
@@ -0,0 +1,25 @@
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+const network = Network.create((operation, variables) =>
+    fetch('http://localhost:4000/', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables,
+        }),
+    }).then(response => response.json()),
+);
+
+const source = new RecordSource();
+const store = new Store(source);
+
+export const env = new Environment({
+    network,
+    store
+});
+
+export default env;
