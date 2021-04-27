0c275223608251c0ad6c6a485097d6d4b83eed24
Guilherme775 <guilhermereis28122002@gmail.com>
2021-04-26 22:18:20 -0300

    feat: finish the layout, and add feed page

--- src/components/ThemeSwitcher/styles.ts
+++ src/components/ThemeSwitcher/styles.ts
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     position: absolute;
     width: fit-content;
-    top: 24px;
-    right: 32px;
+    top: 40px;
+    right: 48px;
+
+    @media screen and (max-width: 576px) {
+        top: 24px;
+        right: 32px;
+    }
 `;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { Login, Register } from "./pages";
+import { Login, Register, Feed } from "./pages";
 
 export const Routes = () => {
   return (
@@ -9,6 +9,7 @@ export const Routes = () => {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/feed" component={Feed} />
       </Switch>
     </BrowserRouter>
   );
